82cd7de2527aaa62f1d0283d82d51b7b2a199ac3
filfilt <tafneg4@gmail.com>
2020-08-14 15:00:16 -0400

    loop

---
+++ Part016 Loop.py
@@ -0,0 +1,35 @@
+#Loop
+
+#While Loop
+#Eg1:
+
+# num = 1
+# while num<10:
+#     print(num)
+#     num += 1
+
+#Eg2:
+
+# num = 1
+# while num<10:
+#     print(num)
+#     if num==5:
+#        break
+#     num+=1
+    
+#For Loop
+
+# name="nega"
+# for n in name:
+#     print(n)
+
+num = ['nega','abebe','belachew']
+for s in num:
+    print(s)
+    
+color = ['red','green','blue']
+size = ['small','medium','large']
+for c in color:
+    for s in size:
+        print(c,s)
+   
