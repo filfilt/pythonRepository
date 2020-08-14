26081b50530fc5bb90cf01562d9870f414fd6d36
filfilt <tafneg4@gmail.com>
2020-08-14 03:22:09 -0400

    Input and Indent

---
+++ Part0010-Input and Indentation.py
@@ -0,0 +1,22 @@
+#Input and Indentation
+#Eg1: Input
+'''
+fname = input("please Enter First Name:")
+lname = input("please Enter last Name:")
+print("Your Full name is:"+fname +" "+lname)
+'''
+#Eg2: Indentation
+'''
+student = ['nega','abebe','zemene']
+for s in student:
+    print(s)
+    '''
+    
+#Eg3:
+age = int(input('Enter age:'))
+if age<13:
+    print('Kid')
+elif age<19:
+    print('teen')
+else:
+    print('Not teen')
