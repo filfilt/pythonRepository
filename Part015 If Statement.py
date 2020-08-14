750fbcd38a719ff6b61a9bbd3c53aa6aaad0277e
filfilt <tafneg4@gmail.com>
2020-08-14 14:04:28 -0400

    If Statement

---
+++ Part015 If Statement.py
@@ -0,0 +1,18 @@
+#If Statement
+
+'''
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+if num1 == num2:
+    print("The two numbers are equal")
+elif num1>num2:
+    print("num1 is greater tnan num2")
+else:
+    print("num1 is less than num2")
+    '''
+num1 = int(input("Enter one number:"))  
+if num1%2 == 0:
+     print("even")
+else:
+    print("Odd") 
+
