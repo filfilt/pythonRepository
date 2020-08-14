4092984cd5ee535e554be99133e887de9279f0cc
filfilt <tafneg4@gmail.com>
2020-08-14 13:47:33 -0400

    Identity & Membership operator

---
+++ Part014 Identity and Membership.py
@@ -0,0 +1,16 @@
+#Identity & Membership
+# Identity: Is  and Is not
+
+'''
+lst1 = []
+lst2 = []
+lst3 = lst1
+print(lst1 is lst2) # False b/s they are in diff place
+print(lst1==lst1) # True b/s they are empty
+print(lst3==lst1) # True they have the same value
+print(lst3 is lst1) # True they are in the same place
+'''
+#Membership
+fruits = ['banana','apple']
+print('orange' in fruits)
+print('orange' not in fruits)
