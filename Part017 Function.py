19ffea8f31a862aa9fb862ab9421de3017d0af10
filfilt <tafneg4@gmail.com>
2020-08-14 15:29:55 -0400

    Function

---
+++ Part017 Function.py
@@ -0,0 +1,20 @@
+#Function
+#Eg1:
+'''
+add = lambda a: a+5
+print(add(5))
+'''
+
+#Eg2:
+'''
+add2num = lambda n1,n2: n1+n2
+print(add2num(12,4))
+'''
+#Eg3:
+addthreenumbers = lambda a,b,c: a+b+c
+
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+num3 = int(input("Enter third number:"))
+
+print(addthreenumbers(num1,num2,num3))
