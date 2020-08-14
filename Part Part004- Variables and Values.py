b13305e2c19c7d7a6f1c3505fca72844949ab2e8
filfilt <tafneg4@gmail.com>
2020-08-14 01:57:53 -0400

    Variables and Values

---
+++ Part Part004- Variables and Values.py
@@ -0,0 +1,15 @@
+#Variables and Values
+
+#Eg1: For Number
+'''
+n1 = 12
+n2 = 13
+n3 = n1 + n2
+print(n3)
+'''
+#Eg2: For string
+name = "nega"
+# print(name)
+# print(name[0])
+#print(name[0:3])
+print(name[-1])
