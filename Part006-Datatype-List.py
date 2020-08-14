bba8cd7446e9a9b606c4582dce92b57da1d813a7
filfilt <tafneg4@gmail.com>
2020-08-14 02:31:41 -0400

    List

---
+++ Part006-Datatype-List.py
@@ -0,0 +1,19 @@
+#Part 6-Datatype-List
+#Eg1:
+'''
+lst1 = [12,'nega','tafere','m',30]
+print(lst)
+'''
+
+#Eg2:
+'''
+lst1 = [12,'nega','tafere','m',30]
+lst2 = ['Abebe','Tigabu']
+print(lst1*2)
+ '''
+ #Eg3:
+
+lst1 = [12,'nega','tafere','m',30]
+lst2 = ['Abebe','Tigabu']
+print(lst1+lst2)
+ 
