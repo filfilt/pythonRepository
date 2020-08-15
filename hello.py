acdca7562bcb97449fda2d8214eacc7648b9918d
filfilt <tafneg4@gmail.com>
2020-08-14 21:55:00 -0400

    Modules

---
+++ hello.py
@@ -0,0 +1,14 @@
+fname="nega"
+lname="tafere"
+def GreetStudents():
+    
+    print("Hello students")
+   
+def getFullName(fn,ln):
+    print(fn+" , "+ln)
+
+student={
+    "Fname":"nega",
+    "Lname":"tafere",
+    "Age":30
+}
---
+++ part24.modules.py
@@ -0,0 +1,6 @@
+#import "hel" in to this 
+import hello as h
+h.GreetStudents()
+print(h.fname+" "+h.lname)
+h.getFullName("nega","tafere")
+print(h.student['Age'])
