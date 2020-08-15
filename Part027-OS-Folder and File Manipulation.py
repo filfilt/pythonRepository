cfc9446af4769dd8800ca9a49bef0dace8a8fd3a
filfilt <tafneg4@gmail.com>
2020-08-15 00:40:54 -0400

    OS-Folder and File Manipulation

---
+++ Part027-OS-Folder and File Manipulation.py
@@ -0,0 +1,14 @@
+#OS-Folder and File Manipulation
+
+import os
+from datetime import datetime
+#os.mkdir("student")
+#os.makedirs('student/Grade/ninthGrade/student.txt')
+
+#print(os.path.dirname("student/Grade/ninthGrade/student.txt"))
+#print(os.path.basename("student/Grade/ninthGrade/student.txt"))
+#print(os.path.exists("student/Grade/ninthGrade/student.txt"))
+#print(os.path.split("student/Grade/ninthGrade/student.txt"))
+print(dir(os))
+#print(dir(os.path))
+os.rename(" "," ")
